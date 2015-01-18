667a0be44e46aa4b115515d06abf7c99880ebb7b
Sebastian Superczynski <sebastian.superczynski@espeo.pl>
2015-01-18 23:14:28 +0100

    hermetization

--- src/Main/FrontendBundle/Resources/public/js/Counters/counter-controller.js
+++ src/Main/FrontendBundle/Resources/public/js/Counters/counter-controller.js
@@ -5,48 +5,15 @@
         .module('war.dashboard')
         .controller('CountersCtrl', CountersCtrl);
 
-    CountersCtrl.$inject = ['$scope', '$interval', 'EndPointFactory', 'SoldierFactory'];
+    CountersCtrl.$inject = ['$scope', 'SoldierService'];
 
-    function CountersCtrl($scope, $interval, EndPointFactory, SoldierFactory) {
+    function CountersCtrl($scope, SoldierService) {
 
         $scope.soldierCounter = function () {
-            return soldierCounter.getCounter();
+            return SoldierService.getCounter();
         };
 
-        var soldierCounter = {
-            amount: 0,
-            factory: {
-                level: 6
-            },
-            getCounter: function () {
-                return Math.round(this.amount);
-            },
-            setCounter: function (data) {
-                this.amount = this.amount + 1 * SoldierFactory.levels[soldierCounter.factory.level];
-            }
-        };
 
-        function init() {
-            EndPointFactory.get()
-                .then(function (res) {
-                    setInitialCounters(res);
-                    $interval(function () {
-                        soldierCounter.setCounter();
-                    }, 1000);
-                });
-        }
-
-        function setInitialCounters(res) {
-            // soldierCounter.amount =
-            // amount from db + ((NOW() - last modification timestamp) *  soldierCounter.factory.map[soldierCounter.factory.level])
-            var now = moment(),
-                last = moment(res.data.date),
-                diff = now.diff(last, 's');
-            soldierCounter.amount = res.data.soldier + diff * SoldierFactory.levels[soldierCounter.factory.level];
-        }
-
-        init();
     }
 
 })();
-
